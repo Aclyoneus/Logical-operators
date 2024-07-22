6468f5b009f7f247f094f8bf88e2bfe697527d5e
Wojciech <wojciechwroobel@gmail.com>
2024-07-22 22:03:55 +0200

    created function checking if one or both are truthy - ex 3

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function isAlarmSet(isEmployed, isOnVacation) {
 }
 
 // 2
+
+// 3
+
+const isOneOrBothTruthy = (a, b) => {
+    return ((a && !b) || (!a && b)) || a && b;
+}
+
+const isOneTruthy = (a, b) => {
+    return (a && !b) || (!a && b);
+}
+
+// 4
